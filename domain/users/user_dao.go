ba2fec664429b2905d8719ead8c76423769c8e30
satyajeet <satyajeet@getvital.in>
2020-03-29 04:18:19 +0530

    proper getting the user and now having mysql errors for better error working

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -10,6 +10,8 @@ import (
 
 const (
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser    = "SELECT id, first_name, last_name, email, date_created  FROM users WHERE id=?;"
+	errorNoRows     = "no rows in result set"
 )
 
 var (
@@ -17,15 +19,18 @@ var (
 )
 
 func (user *User) GET() *errors.RestError {
-	result := userDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf(" user %id not found", user.Id))
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
+	defer stmt.Close()
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
+	}
 
 	return nil
 }
